5da55b06ae06bc42c0b3893efe47d236662eff92
ekvnsu <sc2580@hanmail.net>
2016-01-19 15:12:15 +0900

    Hello.java modified on GitHub

--- Hello.java
+++ Hello.java
@@ -1,11 +1,10 @@
+//For command line git tutorial
+//GitHub modification
 public class Hello{
-public static void main(String[] args)
-	{	
+	public static void main(String[] args){	
 	System.out.println("Hello Git!");
 	System.out.println("What is Git?");
-
 	System.out.println("what's DVCS?");
-
 	System.out.println("Distributed Version Control System");
 
 	}
